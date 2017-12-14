8dbcb3e8bae74526d1a2859cceabc993aa9844a4
Rasmus Bækgaard <rab@netcompany.com>
2017-12-14 09:40:58 +0100

    Add readme.md, and add script

---
+++ GitSetup.ps1
@@ -0,0 +1,257 @@
+#Run the following command with quotations:
+# ./GitSetup "Your name" "Your email"
+
+param(
+	[Parameter(Mandatory=$true)] [string]$name, 
+	[Parameter(Mandatory=$true)] [string]$mail
+)
+
+$global:x86 = $true
+$global:PowerProfile = $PROFILE
+$global:ConfigFile = ".gitconfig"
+$global:P4MergeFile = "P4Merge.zip"
+$global:gitPath = "${env:ProgramFiles(x86)}" + "\Git"
+$global:mergePath = $gitPath + "\libexec\git-core\mergetools"
+$global:installationFolder = $pwd;
+
+function CheckIfMsysIsInstallad
+{
+	# Check if MSysGit has been installed
+	$ValidPath = Test-Path $gitPath
+	If($ValidPath -eq $false)
+	{
+		$global:x86 = $false
+		$global:gitPath = "${env:ProgramFiles}" + "\Git"
+		$global:mergePath = $gitPath + "\mingw64\libexec\git-core\mergetools"
+	}
+
+	$ValidPath = Test-Path $global:gitPath
+	# It has not:
+	If ($ValidPath -eq $false) 
+	{
+		Write-Host "You have not install MSysGit yet. Please do so." -Foreground Green
+		Write-Host "Please hit a key to download it..." -Foreground Green
+		$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+		# Open browser to GitHub to insert key
+		cmd.exe /C start https://git-for-windows.github.io/
+
+		$host.exit
+	}
+}
+
+# Test if .$PROFILE has been created.
+# If not, do so
+function CreateProfile
+{
+	# Check if the profile has been created
+	$GotPath = Test-path $global:PowerProfile
+
+	# Create profile
+	if(-not $GotPath) 
+	{
+		New-item -type file -force $global:PowerProfile
+	}	
+}
+
+# Download prefix for the profile as either x64 or x86 
+function DownloadGit
+{
+	#Paste to $PROFILE
+	if($global:x86 -eq $true)
+	{
+		$text = @"
+`$env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\bin"
+`$env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\cmd"
+`$env:home = `$env:userprofile
+
+#Alias
+Set-Alias g git
+
+"@
+
+		$text | out-file $global:PowerProfile
+	}
+	else #x64
+	{
+		$text = @"
+`$env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\bin"
+`$env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\cmd"
+`$env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\usr\bin"
+`$env:home = `$env:userprofile
+ 
+#Alias
+Set-Alias g git 
+
+"@
+
+		$text | out-file $global:PowerProfile
+	}
+}
+
+
+function SetupSSH
+{
+	try 
+	{
+		# Generate an ssh-key to use
+		Write-Host "If you want a password enter one. Else leave blank" -Foreground Green
+
+		$sshPath = (Get-Item "Env:userprofile").Value + "\.ssh"
+		$sshKey = $sshPath + "\id_rsa"
+
+		if((Test-Path $sshPath) -eq $false)
+		{
+			New-item $sshPath -itemType directory
+			Set-itemProperty -Path $sshPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+		}
+		ssh-keygen.exe -f $sshKey		
+	}
+	catch
+	{
+		Write-Host "Something went wrong when you tried to setup your SSH-key"
+	}
+}
+
+#Download content for the ~/.gitconfig file
+function DownloadGitconfigContent
+{
+	try
+	{
+		# Download content and paste to .gitconfig
+		Write-Host "Overriding the .gitconfig file. It can be found in $env:userprofile\$global:ConfigFile"
+		cp "$global:installationFolder\gitconfig.txt" "$env:userprofile\$global:ConfigFile"
+		
+		# Insert your name and email into .gitconfig
+		git config --global user.name "$name"
+		git config --global user.email "$mail"
+	}
+	catch {
+		Write-Host "Can't use Git - No clue to why this is happening"
+	}
+}
+
+function SetupMergeTool
+{
+	try
+	{
+	    # Extract it
+	    Write-Host "Installing P4Merge as mergetool"
+	    $shell = new-object -com shell.application
+	    $zip = $shell.NameSpace("$pwd\$global:P4MergeFile")
+	    foreach($item in $zip.items())
+	    {
+		    $shell.Namespace($mergePath).copyhere($item, 0x14)
+	    }
+	}
+	catch	
+    {
+		Write-Host "Are you sure you are in Admin mode?" -ForegroundColor Green
+	}
+}
+	
+#If needed - Posh-git cannot be downloaded without
+function SetupProxy
+{
+	# Set proxy, if any
+	$proxies = (Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').proxyServer
+
+	if ($proxies)
+	{
+		if ($proxies -ilike "*=*")
+		{
+			$proxies -replace "=","://" -split(';') | Select-Object -First 1
+		}
+
+		else
+		{
+			"http://" + $proxies
+		}
+	} 
+
+	if($proxies)
+	{
+		git config --global http.proxy $proxies
+	}
+}
+
+function SetupPostGit
+{
+	try 
+    {
+		# Download and install posh-git
+		Write-Host "Download and install post-git"
+        $pathToInstall = Split-Path -Path $mergePath -Parent
+
+        $currentFolder = $pwd
+
+        cd $pathToInstall
+
+        If((Test-Path posh-git) -eq $false)
+        {
+           Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Confirm
+
+	        git clone -q https://github.com/dahlbyk/posh-git.git
+	        cd posh-git
+            ./install.ps1	
+        }
+        cd $currentFolder
+	}
+	catch {
+		Write-Host "While downloading and installing posh-git, something went wrong!" -Foreground Red
+	}
+}
+
+function DownloadGitconfigContent2
+{
+	#Colors for PowerShell
+	$text = @"
+`$Global:GitPromptSettings.BeforeIndexForegroundColor        = [ConsoleColor]::Green
+`$Global:GitPromptSettings.IndexForegroundColor              = [ConsoleColor]::Green
+`$Global:GitPromptSettings.WorkingForegroundColor            = [ConsoleColor]::Red
+`$Global:GitPromptSettings.LocalWorkingStatusForegroundColor = [ConsoleColor]::Red
+"@
+
+	$text | out-file $global:PowerProfile -append
+}
+
+function GetSSHKey
+{
+	# Copy your ssh-key
+	$keyPath = (Get-Item "Env:userprofile").Value + "/.ssh/id_rsa.pub"
+	Get-Content $keyPath | clip	
+
+    Write-Host "" #Empty line
+	Write-Host "Your SSH-key is in you paste-bin. Use 'Ctrl+V' to paste it into the browser." -Foreground Green
+
+	Write-Host "Go register on https://github.com/settings/ssh if you use that :)" -Foreground Green
+}
+
+CheckIfMsysIsInstallad
+CreateProfile
+DownloadGit
+
+# Reload profile to get the Git commands
+.$PROFILE
+
+DownloadGitconfigContent
+SetupSSH
+SetupMergeTool
+SetupProxy
+SetupPostGit
+
+# Go to the home folder
+cd $env:userprofile
+
+# Reload the profile
+.$PROFILE
+
+DownloadGitconfigContent2
+GetSSHKey
+
+.$PROFILE
+
+Write-Host "To get your ssh-key in your paste-bin, write the follwing command:"
+Write-Host "Get-Content ~/.ssh/id_ras.pub | clip "
+
+cd $global:installationFolder
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # GitSetup
 This repository will setup Git for shell warriors. It includes setup of Posh-git, P4Merge, and several shortcuts
+
+* Add a PowerShell profile, if you do not have one. This gives you the shortcut ``g`` for ``git``.
+* Tell PowerShell where Git is located, so the commands can be used
+* Override the ``~/.gitconfig`` file with a whole lot of new aliases, and set it up to default use P4Merge. Open ``~/.gitconfig`` in your favorite editor (note MS Notepad) to see all of these.
+* Install a light version of P4Merge 2015.2
+* Install [Posh-gi](https://github.com/dahlbyk/posh-git) for better overview of Git in PowerShell
+* Setup a proxy for Git, extracting the data from Internet Explorer (should there be any)
+* Setup an SSH-key, which is located under ``~/.ssh/id_rsa.pub`` (use to authorize you with TFS/GitHub/BitBucket/TeamCity/Jenkins/GitLab/...)
+
+
+## Step 1 - Installing Git:
+* Download Git from the following link:
+	https://git-for-windows.github.io/
+* Install it with the following parameters:
+	* Check the following under 'Selected Components', and uncheck everything else unless needed
+		* Git LFS
+		* Use a TrueType font in all console windows
+	* Select the following radio button under 'Adjusting your PATH environment'
+		* Use Git from the Windows Command Prompt
+	* Select the following radio button under 'HTTPS transport backend'
+		* Use the OpenSSL library
+	* Select the following radio button under 'Configuring the line ending conversions'
+		* Checkout as-is, commit as-is
+	* Select the following radio button under 'Configuring the terminal emulator to use with Git Bash'
+		* Use Windows' default console window
+	* The check boxes under 'Configuring extra options' are optional
+
+
+## Step 2 - Running the script:
+
+Running the GitSetup.ps1-file will setup the following for you
+
+(While in Administrator mode)
+* ``cd`` to this folder
+	* Can be accomplished by typing 'Powershell' in the folder path in the top of the explorer
+
+* Run the following command including the quotation marks, but with your name and email:
+
+	 ``./GitSetup "Your name" "Your email"``
+
+
+
